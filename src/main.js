b6db9424d42bf4c57cd6a8c444c60ea80612d1bc
Luiz Neto <luizneto.dev@outlook.com>
2026-02-11 14:07:05 -0300

    feat: modularizacao arquivos CSS

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./styles/layout.css";
+import "./styles/variables.css";
 
-document.querySelector('#app').innerHTML = `
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +21,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
